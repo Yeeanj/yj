b11484373e4d66cf57e396ffda2ce9e3f8efd6b1
Yeeanj <sesju0527@gamil.com>
2023-09-25 13:21:06 +0900

    4.함수

--- 07-javascript/4-함수.js
+++ 07-javascript/4-함수.js
@@ -37,3 +37,75 @@ function hello4() {
 }
 
 console.log(hello1(), hello2(), hello3(), hello4());
+
+// 예시
+function getGrade(score) {
+  if (score === 100) {
+    return "A+"; // A+
+  } else if (score >= 90) {
+    return "A"; // A
+  } else if (score >= 80) {
+    return "B"; // B
+  } else if (score >= 70) {
+    return "C"; // C
+  } else if (score >= 60) {
+    return "D"; // D
+  } else {
+    return "F"; // F
+  }
+}
+
+console.log(getGrade(55));
+
+// 매개변수 패턴
+function sum(x, y = 1) {
+  return x + y;
+}
+
+console.log(sum(1, 5));
+console.log(sum(1));
+
+//  객체의 구조 분해 할당
+const user = {
+  name: "winter",
+  age: 2,
+  // email: "seaju0527@naver.com",
+};
+
+function getName({ name }) {
+  return name;
+}
+function getEmail({ email = "이메일이 없습니다" }) {
+  return email;
+}
+
+console.log(getName(user));
+console.log(getEmail(user));
+
+// 배열의 구조 분해 할당
+const animals = ["dog", "cat", "fish"];
+
+// 두번째 요소 변환하는 기능
+function getSecond([, b = "두 번째 요소가 없습니다.", c]) {
+  return [b, c];
+}
+
+console.log(getSecond(animals));
+
+// 화살표 함수
+// function 함수이름(매개변수){실행문}
+// (매개변수) => {실행문}
+
+function hello() {
+  console.log("hello");
+}
+const hello12 = function () {
+  console.log("hello2");
+};
+const hello13 = () => {
+  console.log("hello3");
+};
+
+hello();
+hello12();
+hello13();
